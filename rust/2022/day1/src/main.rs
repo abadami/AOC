f07eab1a4dadb90b28f2b13a3ff20b5d7f881ae6
andrew.badami <andrew.badami@spreetail.com>
2022-11-30 16:56:03 -0600

    Initial setup for Advent of Code

---
+++ rust/2022/day1/src/main.rs
@@ -0,0 +1,9 @@
+use day1::{part1, part2};
+
+fn main() {
+    println!("--2022 day 01 solution--");
+
+    println!("Part 1: {}", part1());
+
+    println!("Part 2: {}", part2());
+}
---
+++ typescript/2022/main.ts
@@ -0,0 +1,4 @@
+import { part1 } from "./day1/day1.ts";
+
+console.log(`--2022 day 01 solution--`);
+console.log(`Part 1: ${part1()}`);
