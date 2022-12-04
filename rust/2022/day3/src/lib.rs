133b95afcbd37aaf1bb236b92f0b676c6dac21d3
abadami <andrewbadami@gmail.com>
2022-12-03 20:14:59 -0600

    Rust Day 3 Setup

---
+++ rust/2022/day3/src/lib.rs
@@ -0,0 +1,15 @@
+use std::collections;
+
+pub fn part1(input: &String) -> i64 {
+  let scenarios: Vec<&str> = input.split("\r\n").collect();
+
+  scenarios.iter();
+
+  return 0;
+}
+
+pub fn part2(input: &String) -> i64 {
+  let scenarios: Vec<&str> = input.split("\r\n").collect();
+
+  return 0;
+}
