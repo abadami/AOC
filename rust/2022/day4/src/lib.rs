9e67927f78516af8ddc0b2209003be117ca11dd2
abadami <andrewbadami@gmail.com>
2022-12-04 17:17:00 -0600

    Day 4, part 2 Rust

--- rust/2022/day4/src/lib.rs
+++ rust/2022/day4/src/lib.rs
@@ -16,12 +16,30 @@ fn is_fully_contained(scenario: Vec<i64>) -> i64 {
   return 0;
 }
 
+fn count_overlap_sections(scenario: Vec<i64>) -> i64 {
+  if scenario.len() != 4 {
+    panic!("This isn't right");
+  }
+
+  let mut elf_a_sections: Vec<i64> = Vec::new();
+
+  for i in scenario[0]..scenario[1] + 1 {
+    elf_a_sections.push(i);
+  }
+
+  for i in scenario[2]..scenario[3] + 1 {
+    if elf_a_sections.contains(&i) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
 pub fn part1(input: &String) -> i64 {
   return input.lines().into_iter().map(get_ids).map(is_fully_contained).sum();
 }
 
 pub fn part2(input: &String) -> i64 {
-  let scenarios: Vec<&str> = input.split("\r\n").collect();
-
-  return 0;
+  return input.lines().into_iter().map(get_ids).map(count_overlap_sections).sum();
 }
