fa6f6426e27ccabe96f5ec1847105b205369c3f1
andrew.badami <andrew.badami@spreetail.com>
2022-12-06 09:36:49 -0600

    Rust Day 6

---
+++ rust/2022/day6/src/lib.rs
@@ -0,0 +1,34 @@
+use std::collections::HashSet;
+
+fn look_for_distinct_characters_index(characters: Vec<char>, number: usize) -> usize {
+  let mut index = 0;
+
+  while index <= characters.len() - number {
+    let mut character_set: HashSet<char> = HashSet::new();
+
+    for i in 0..number {
+      character_set.insert(characters[index + i]);
+    }
+
+    if character_set.len() == number {
+      // answer is index right before
+      return index + number;
+    }
+
+    index = index + 1;
+  }
+
+  return 0;
+}
+
+pub fn part1(input: &String) -> usize {
+  let characters: Vec<char> = input.chars().collect();
+
+  return look_for_distinct_characters_index(characters, 4);
+}
+
+pub fn part2(input: &String) -> usize {
+  let characters: Vec<char> = input.chars().collect();
+
+  return look_for_distinct_characters_index(characters, 14);
+}
