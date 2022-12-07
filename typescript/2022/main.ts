7679f18083c45e188048fee447e10052d6109ddd
andrew.badami <andrew.badami@spreetail.com>
2022-12-07 10:25:54 -0600

    Day 7, Typescript

--- typescript/2022/main.ts
+++ typescript/2022/main.ts
@@ -2,6 +2,7 @@ import { part1 as day1Part1, part2 as day1Part2 } from "./day1/day1.ts";
 import { part1 as day2Part1, part2 as day2Part2 } from "./day2/day2.ts";
 import { part1 as day3Part1, part2 as day3Part2 } from "./day3/day3.ts";
 import { part1 as day4Part1, part2 as day4Part2 } from "./day4/day4.ts";
+import { part1 as day7Part1, part2 as day7Part2 } from "./day7/day7.ts";
 
 console.log(`--2022 day 01 solution--`);
 const day1Input = Deno.readTextFileSync("./day1/input.txt");
@@ -22,3 +23,8 @@ console.log(`--2022 day 04 solution--`);
 const day4Input = Deno.readTextFileSync("./day4/input.txt");
 console.log(`Part 1: ${day4Part1(day4Input)}`);
 console.log(`Part 2: ${day4Part2(day4Input)}`);
+
+console.log(`--2022 day 07 solution--`);
+const day7Input = Deno.readTextFileSync("./day7/input.txt");
+console.log(`Part 1: ${day7Part1(day7Input)}`);
+console.log(`Part 2: ${day7Part2(day7Input)}`);
