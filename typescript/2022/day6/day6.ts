b3b71ac6522186f4cb728b9c4f55e8545c192e61
abadami <andrewbadami@gmail.com>
2022-12-10 17:32:55 -0600

    Day 6, Typescript

--- typescript/2022/day6/day6.ts
+++ typescript/2022/day6/day6.ts
@@ -1,7 +1,25 @@
+const findDistinctCharactersIndex = (characters: string[], number: number) => {
+  for (let i = 0; i <= characters.length - number; i++) {
+    const characterSet = new Set();
+
+    for (let j = 1; j <= number; j++) {
+      characterSet.add(characters[i + j]);
+    }
+
+    if (characterSet.size === number) {
+      return i + number + 1;
+    }
+  }
+};
+
 export const part1 = (input: string) => {
-  return "To Be Completed";
+  const characters = input.split("");
+
+  return findDistinctCharactersIndex(characters, 4);
 };
 
 export const part2 = (input: string) => {
-  return "To Be Completed";
+  const characters = input.split("");
+
+  return findDistinctCharactersIndex(characters, 14);
 };
