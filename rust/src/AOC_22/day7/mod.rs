207a031ad1833749a2443b42c35972a31134028c
abadami <andrewbadami@gmail.com>
2023-02-28 22:58:53 -0600

    Hmm, something is off here

--- rust/src/AOC_22/day7/mod.rs
+++ rust/src/AOC_22/day7/mod.rs
@@ -70,11 +70,15 @@ impl Graph {
         "cd" => {
           match command_pieces[1] {
             ".." => {
-              let new_previous_index = self
+              let new_previous = self
               .edges
               .iter()
-              .position(|edge| edge.target_node == previous_node_index)
-              .unwrap_or_default();
+              .find(|edge| edge.target_node == previous_node_index);
+
+              let new_previous_index = match new_previous {
+                None => 0,
+                Some(edge) => edge.main_node
+              };
 
               current_node_index = previous_node_index;
               previous_node_index = new_previous_index;
@@ -102,30 +106,77 @@ impl Graph {
 
     }
   }
+
+  fn calculate_node_size(&self, node_index: NodeIndex) -> i32 {
+    let filtered = self.edges.iter().filter(|edge| edge.main_node == node_index);
+    let mut new_size = 0;
+
+    for edge in filtered {
+      let node = &self.nodes[edge.target_node];
+
+      let size = match node.is_dir {
+        true => self.calculate_node_size(edge.target_node),
+        false => node.size
+      };
+
+      new_size += size;
+    }
+
+    new_size
+  }
 }
 
-
-
-fn part1(input: &str) -> i64 {
+fn part1(input: &str) -> i32 {
   let mut directory = Graph::new();
 
   directory.initialize_tree(input);
 
-  for node in directory.nodes.iter() {
-    println!("Name: {0}, size: {1}, Is Directory? {2}", node.name, node.size, node.is_dir);
+  for index in 0..directory.nodes.len() {
+    if directory.nodes[index].is_dir {
+      directory.nodes[index].size = directory.calculate_node_size(index);
+    }
   }
 
-  for edge in directory.edges.iter() {
-    let main_node = &directory.nodes[edge.main_node];
-    let target_node = &directory.nodes[edge.target_node];
-    println!("Main: {0}, Target: {1}", main_node.name, target_node.name);
+  let mut answer = 0;
+
+  for node in directory.nodes {
+    if node.is_dir && node.size <= 100000 {
+      answer += node.size;
+    }
   }
 
-  0
+  answer
 }
 
-fn part2(input: &str) -> i64 {
-  0
+fn part2(input: &str) -> i32 {
+  let mut directory = Graph::new();
+
+  directory.initialize_tree(input);
+
+  for index in 0..directory.nodes.len() {
+    if directory.nodes[index].is_dir {
+      directory.nodes[index].size = directory.calculate_node_size(index);
+    }
+  }
+
+  let system_size = directory.nodes[0].size;
+  let system_remainder = 70000000 - system_size;
+  let needed_size = 30000000 - system_remainder;
+
+  directory.nodes.sort_by(|a, b| a.size.cmp(&b.size));
+
+  println!("System Current: {0}, Remaining Space: {1}, Needed: {2}", system_size, system_remainder, needed_size);
+
+  let mut answer = i32::MAX;
+
+  for node in directory.nodes {
+    if node.is_dir && node.size > needed_size && node.size < answer {
+      answer = node.size;
+      break;
+    }
+  }
+
+  answer
 }
 
 pub fn run_problem7() {
